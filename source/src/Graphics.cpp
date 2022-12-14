6019fd7bc5e887880bd2f6f38b7bbb31c9e925f7
chriskontsis <cak0927@gmail.com>
2022-12-14 09:52:57 -0700

    finishing setting up sdl2 for mac

--- source/src/Graphics.cpp
+++ source/src/Graphics.cpp
@@ -3,5 +3,10 @@
 
 
 Graphics::Graphics() {
+    SDL_CreateWindowAndRenderer(640,480, 0, &window, &renderer);
+    SDL_SetWindowTitle(window, "Cave Story");
+}
 
+Graphics::~Graphics() {
+    SDL_DestroyWindow(window);
 }
--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,3 +1,18 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include "graphics.h"
+
 int main() {
-    
+    Graphics graphics;
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+    return 0;
 }
+  
